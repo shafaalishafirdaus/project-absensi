13aebacaf7848cc7d1449be47a2405414e34bc0f
Fadhlan29 <fadlan.jerad@gmail.com>
2026-04-24 23:44:50 +0700

    updates

--- resources/views/dashboard/scan.blade.php
+++ resources/views/dashboard/scan.blade.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <title>Scan Absensi</title>
     <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css" rel="stylesheet">
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
+    {{-- <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet"> --}}
     <script src="https://unpkg.com/html5-qrcode"></script>
     <link rel="stylesheet" href="{{ asset('css/style.css') }}">
 
